6b49b11f24093faf11a991243d053a108ee6a69b
jeffmorice <jmorice.anim@gmail.com>
2018-11-28 22:28:35 -0600

    Initial commit.

---
+++ CheeseMVC/Controllers/CheeseController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CheeseMVC.Models;
+
+namespace CheeseMVC.Controllers
+{
+    public class CheeseController : Controller
+    {
+        static private List<Cheese> Cheeses = new List<Cheese>();
+
+        public IActionResult Index()
+        {
+           ViewBag.cheeses = Cheeses;
+
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult Remove()
+        {
+            ViewBag.cheeses = Cheeses;
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/cheese/add")]
+        public IActionResult NewCheese(string name, string description)
+        {
+            //add the new cheese to my existing cheeses
+            Cheeses.Add(new Cheese(name, description));
+            return Redirect("/cheese");
+        }
+
+        [HttpPost]
+        [Route("/cheese/remove")]
+        public IActionResult RemoveCheese(string[] selected)
+        {
+            //remove the selected cheeses from my existing cheeses
+            foreach(string selection in selected)
+            {
+                Cheese toBeRemoved = null;
+
+                foreach (Cheese cheese in Cheeses)
+                {
+                    if (selection == cheese.Name)
+                    {
+                        toBeRemoved = cheese;
+                    }
+                }
+
+                Cheeses.Remove(toBeRemoved);
+            }
+
+            return Redirect("/cheese");
+        }
+    }
+}
---
+++ CheeseMVC/Models/Cheese.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.Models
+{
+    public class Cheese
+    {
+        private string name;
+        private string description;
+
+        public Cheese(string pName, string pDescription)
+        {
+            name = pName;
+            description = pDescription;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+    }
+}
